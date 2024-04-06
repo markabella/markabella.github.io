a234c88c20b77099eedfcb7707753bf70a89e2fa
Mark Abella <mark.abella@gmail.com>
2024-04-06 10:49:39 -0500

    Update script.js
    update

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function submitQuestion() {
   const userQuestion = questionBox.value.trim();
 
  fetch(`https://scintillating-pika-68754f.netlify.app/.netlify/functions/yourFunction?q=${encodeURIComponent(userQuestion)}`)
-.then(response => response.json())
         .then(response => response.json())
         .then(data => {
             responseElement.innerText = `Answer: ${data.answer}`;
