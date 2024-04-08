618d17aa7cd90da209bfe5dbb11c0a3d1ab00b90
Mark Abella <mark.abella@gmail.com>
2024-04-08 18:13:01 -0500

    Update script.js

--- ladder/script.js
+++ ladder/script.js
@@ -2,6 +2,8 @@ function submitQuestion() {
     
     const questionBox = document.getElementById('userQuestion');
     const userQuestion = questionBox.value.trim();
+    const searchInLadderCheckbox = document.getElementById('searchBookOption');
+    const searchInLadder = searchInLadderCheckbox.checked;
     const responseElement = document.getElementById('response');
     const ladderLoader = document.getElementById('ladderLoader');
     const submitButton = document.querySelector("button");
@@ -11,7 +13,7 @@ function submitQuestion() {
     submitButton.disabled = true;
     ladderLoader.classList.remove('hidden');
 
-    fetch(`https://scintillating-pika-68754f.netlify.app/.netlify/functions/answerQuestion?q=${encodeURIComponent(userQuestion)}`)
+    fetch(`https://scintillating-pika-68754f.netlify.app/.netlify/functions/answerQuestion?q=${encodeURIComponent(userQuestion)}&searchInLadder=${searchInLadder}`)
         .then(response => {
             if (!response.ok) {
                 // If response from server is not OK, throw an error with status text
@@ -21,10 +23,7 @@ function submitQuestion() {
         })
         .then(data => {
             console.log('API Response:', data); // Log the entire response for debugging
-            // Use optional chaining and nullish coalescing to handle cases where data or data.answer might be undefined
             const answer = data?.answer ?? "No answer provided."; // Provide a default message if `data.answer` is undefined
-            // replyNotice.style.display = 'none';
-            // Hide the ladder loader
             ladderLoader.classList.add('hidden');
             responseElement.innerText = `Answer: ${answer}`;
             submitButton.disabled = false; // Re-enable button upon completion
@@ -35,7 +34,6 @@ function submitQuestion() {
             responseElement.innerText = `Error fetching response: ${error.message}`;
             submitButton.disabled = false;
         });
-     
 }
 
 document.getElementById('userQuestion').addEventListener('keypress', function(event) {
