b61cc677d0cdf701fa8fbf609faa424ad0e14322
markabella <mark.abella@gmail.com>
2024-04-12 23:35:28 -0500

    Copy test multiplayer subdirectory

---
+++ test/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1712982732,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/aimspot-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/peer-sheet1.png",
+		"images/peerlaser-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
